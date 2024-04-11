0bdf1b835819cdc13cf15a56f23ce43df4a179b1
Christopher <41389677+Zenif-NIght@users.noreply.github.com>
2024-04-11 12:44:34 -0600

    color and angle

--- assets/URDF_loader/spot_robot.js
+++ assets/URDF_loader/spot_robot.js
@@ -11,7 +11,7 @@ import { URDFLoader } from './URDFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(80, 1, 0.01, 1000);
 // const camera = new THREE.OrthographicCamera();
-camera.position.set(0.6, 0.2, 0);
+camera.position.set(0.9, 0.3, 0);
 
 const camLookat = new THREE.Vector3(0, 0, 0);
 camera.lookAt(camLookat);
@@ -56,9 +56,9 @@ camera.updateProjectionMatrix();
 const directionalLight = new THREE.DirectionalLight(0x00FFFF, 0.5);
 directionalLight.position.set(1, 1, -1); // x is right, y is up, z is towards the cam
 
-const pointLight = new THREE.PointLight(0x00FF00);
+const pointLight = new THREE.PointLight(0xFFFF00);
 pointLight.position.set(3, 3, -3);
-const pointLight2 = new THREE.PointLight(0x00BFFF);
+const pointLight2 = new THREE.PointLight(0xe86aa9);
 pointLight2.position.set(3, 3, 3);
 
 scene.add(directionalLight);
@@ -107,10 +107,10 @@ loader2.load(
     // console.dir();
     scene.add(r);
     robot = r;
-    // robot.joints["FR_hip_joint"].setJointValue(-0.2);
-    // robot.joints["FL_hip_joint"].setJointValue(0.2);
-    // robot.joints["RR_hip_joint"].setJointValue(-0.2);
-    // robot.joints["RL_hip_joint"].setJointValue(0.2);
+    robot.joints["fr_hy"].setJointValue(-0.2);
+    robot.joints["fl_hy"].setJointValue(0.2);
+    robot.joints["hr_hy"].setJointValue(-0.2);
+    robot.joints["hl_hy"].setJointValue(0.2);
 
   }, undefined, error => {
     console.log("error loading go1_robot");
@@ -145,29 +145,17 @@ function animate() {
   const ratio4 = Math.max(0, Math.sin(time + (3 * Math.PI / 2)));
   // console.log(ratio);
   if (robot != undefined) {
-    // robot.joints["FR_thigh_joint"].setJointValue(THREE.MathUtils.lerp(1, 0.5, ratio1));
-    // robot.joints["FL_thigh_joint"].setJointValue(THREE.MathUtils.lerp(1, 0.5, ratio3));
-    // robot.joints["RR_thigh_joint"].setJointValue(THREE.MathUtils.lerp(1.2, 0.8, ratio3));
-    // robot.joints["RL_thigh_joint"].setJointValue(THREE.MathUtils.lerp(1.2, 0.8, ratio1));
+    robot.joints["fl_hy"].setJointValue(THREE.MathUtils.lerp(1, 0.5, ratio1));
+    robot.joints["fr_hy"].setJointValue(THREE.MathUtils.lerp(1, 0.5, ratio3));
+    robot.joints["hl_hy"].setJointValue(THREE.MathUtils.lerp(1.2, 0.8, ratio3));
+    robot.joints["hr_hy"].setJointValue(THREE.MathUtils.lerp(1.2, 0.8, ratio1));
 
-    // robot.joints["FR_calf_joint"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio1));
-    // robot.joints["FL_calf_joint"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio3));
-    // robot.joints["RR_calf_joint"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio3));
-    // robot.joints["RL_calf_joint"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio1));
-    // robot.joints["FR_calf_joint"].setJointValue(THREE.MathUtils.lerp(-100, -30, ratio) * DEG2RAD);
+    robot.joints["fl_kn"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio1));
+    robot.joints["fr_kn"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio3));
+    robot.joints["hl_kn"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio3));
+    robot.joints["hr_kn"].setJointValue(THREE.MathUtils.lerp(-2., -1.2, ratio1));
+    robot.joints["fl_kn"].setJointValue(THREE.MathUtils.lerp(-100, -30, ratio) * DEG2RAD);
 
-//     fl_hx: URDFJoint {isObject3D: true, id: 45, uuid: "5d8433f8-c938-49f8-8153-e011373ea042", name: "fl_hx", type: "URDFJoint", …}
-// fl_hy: URDFJoint {isObject3D: true, id: 46, uuid: "196f48ca-63c9-4476-9512-9465a592719d", name: "fl_hy", type: "URDFJoint", …}
-// fl_kn: URDFJoint {isObject3D: true, id: 47, uuid: "84da5c3d-c966-446d-a280-6f7468d53eab", name: "fl_kn", type: "URDFJoint", …}
-// fr_hx: URDFJoint {isObject3D: true, id: 48, uuid: "931f0428-a0fb-41fe-b7fb-9f0dd3f59460", name: "fr_hx", type: "URDFJoint", …}
-// fr_hy: URDFJoint {isObject3D: true, id: 49, uuid: "133934f5-05b8-4fc1-bf1a-ed3f205c0f4a", name: "fr_hy", type: "URDFJoint", …}
-// fr_kn: URDFJoint {isObject3D: true, id: 50, uuid: "4b41bbcb-1c28-4d57-b1b0-11b55ecc572e", name: "fr_kn", type: "URDFJoint", …}
-// hl_hx: URDFJoint {isObject3D: true, id: 51, uuid: "ceba81f7-7cd3-42d9-9fee-cd783314bc60", name: "hl_hx", type: "URDFJoint", …}
-// hl_hy: URDFJoint {isObject3D: true, id: 52, uuid: "638eb3f5-9319-4cbf-bca8-8efc181f53f0", name: "hl_hy", type: "URDFJoint", …}
-// hl_kn: URDFJoint {isObject3D: true, id: 53, uuid: "eaba2e76-d0d2-46ea-9e08-85b6ae718517", name: "hl_kn", type: "URDFJoint", …}
-// hr_hx: URDFJoint {isObject3D: true, id: 54, uuid: "34b10c83-a47c-44d1-b235-e775db623068", name: "hr_hx", type: "URDFJoint", …}
-// hr_hy: URDFJoint {isObject3D: true, id: 55, uuid: "cee8f508-c2b3-4bcd-ab2b-9062a4132af7", name: "hr_hy", type: "URDFJoint", …}
-// hr_kn: URDFJoint {isObject3D: true, id: 56, uuid: "29c237dc-025d-4637-b0b7-0b43c4d7d487", name: "hr_kn", type: "URDFJoint", …}
   }
 
 
